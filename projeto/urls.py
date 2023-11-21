c9090b8edfc30decd3cadcbdfc8d7a6cdb1055b8
Fernando Almeida <fernando.oab81@outlook.com>
2023-11-21 18:49:38 -0300

    app

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def my_view(request):
-    return HttpResponse('Uma linda String')
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view )
+    path('', home ),#HOME
+    path('sobre/', sobre ),#HOME
+    path('contato/', contato ),#HOME
 ]
